a8659ca18d2664db00ad69f41d7dad485aa21bed
manicharanreddy <manicharanreddy@example.com>
2026-02-22 15:17:42 +0530

    Remove conflicting 404 handler

--- backend-server/server.js
+++ backend-server/server.js
@@ -245,11 +245,6 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!' });
 });
 
-// 404 handler
-app.use('*', (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
